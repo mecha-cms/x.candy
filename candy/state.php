2f2336e51048c0bb76bc8c7ba13c8c87e944bb7c
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-10-16 12:52:20 +0700

    Update state.php

--- candy/state.php
+++ candy/state.php
@@ -5,8 +5,6 @@ return [
         // Create your custom candy syntax here…
         'asset' => To::URL(constant('ASSET')),
         'mecha' => '<a href="//mecha-cms.com">Mecha CMS</a>',
-        // Or, override the default candy syntax here…
-        // 'language' => $language
     ],
     'x' => [
         '_COOKIE' => false,
